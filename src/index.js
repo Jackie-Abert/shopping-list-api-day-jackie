9a7a7b2b29746c871eeb3761efbc22dbc53f6cbc
JackieBishop <jax.abert@gmail.com>
2020-07-14 14:59:42 -0400

    test push

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+/* eslint-env jquery */
+/* eslint-disable no-console */
+
 import $ from 'jquery';
 
 import 'normalize.css';
 import './index.css';
 
+import api from './api';
+import store from './store';
 import shoppingList from './shopping-list';
 
 const main = function () {
+  api.getItems()
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
